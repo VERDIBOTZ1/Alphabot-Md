d5be4fc13237d055e662889e6a0600ff35477a2d
Verdi botz <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-01-05 02:38:15 +0700

    Update confat.js

--- confat.js
+++ confat.js
@@ -3,10 +3,10 @@ const chalk = require('chalk')
 const setting = JSON.parse(fs.readFileSync('./setting.json'));
 
 // Other
-global.owner = ["62887435047326","62887435047326"]
-global.packname = 'ZeeoneOfc'
-global.youtube = 'https://youtube.com/c/ZeeoneOfc'
-global.author = '© Alphabot MD'
+global.owner = ["6283869803330","6283869803330"]
+global.packname = 'VERDI BOTZ'
+global.youtube = 'ga puya :>'
+global.author = '© VERDI BOTZ1'
 global.sessionName = 'session'
 global.flaming = "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text="
 global.prefa = ['','!','.','🐦','🐤','🗿']
